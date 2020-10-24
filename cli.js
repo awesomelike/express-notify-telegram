8ec5334661454b17f980c77852bb7d7e5fa8fdde
awesomelike <abdullaev.dev@gmail.com>
2020-10-24 20:42:27 +0500

    handle forbidden error

--- cli.js
+++ cli.js
@@ -2,7 +2,33 @@
 const inquirer = require('inquirer');
 const https = require('https');
 const ora = require('ora');
+const parseChunk = require('./src/utils/parseChunk');
 const { printError } = require('./src/utils/print');
+
+const handleAnswers = (answers) => {
+  const { botToken, channelUsername } = answers;
+  const url = `https://api.telegram.org/bot${botToken}/getChat?chat_id=@${channelUsername}`;
+  const spinner = ora('Fetching ChatId...').start();
+  https.get(url, (res) => {
+    res.on('data', (data) => {
+      spinner.stop();
+
+      const response = parseChunk(data);
+      if (!response.ok) return printError('Invalid botToken or channel username provided!');
+      return console.log('ChatId of the channel:', response.result.id);
+    });
+    res.on('error', (error) => printError(`Telegram server error: ${error}`));
+  });
+};
+
+const handleError = (error) => {
+  if (error.isTtyError) {
+    console.log('Prompt couldn\'t be rendered in the current environment');
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else when wrong
+  }
+};
 
 inquirer
   .prompt([
@@ -17,26 +43,5 @@ inquirer
       name: 'channelUsername',
     },
   ])
-  .then((answers) => {
-    const { botToken, channelUsername } = answers;
-    const url = `https://api.telegram.org/bot${botToken}/getChat?chat_id=@${channelUsername}`;
-    const spinner = ora('Fetching ChatId...').start();
-    https.get(url, (res) => {
-      res.on('data', (data) => {
-        spinner.stop();
-
-        const response = JSON.parse(data.toString());
-        if (!response.ok) return printError('Invalid botToken or channel username provided!');
-        return console.log('ChatId of the channel:', response.result.id);
-      });
-      res.on('error', (error) => printError(`Telegram server error: ${error}`));
-    });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log('Prompt couldn\'t be rendered in the current environment');
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
+  .then(handleAnswers)
+  .catch(handleError);
--- src/sendMessage.js
+++ src/sendMessage.js
@@ -1,8 +1,17 @@
 const https = require('https');
+const parseChunk = require('./utils/parseChunk');
+const { printError } = require('./utils/print');
 
 const sendMessage = (botToken, query) => {
   const API = `https://api.telegram.org/bot${botToken}/sendMessage?${query}`;
-  https.get(API);
+  https.get(API, (res) => {
+    res.on('data', (data) => {
+      const response = parseChunk(data);
+      if (!response.ok) {
+        printError(`The notification got ignored. Reason: ${response.description}`);
+      }
+    });
+  });
 };
 
 module.exports = sendMessage;
