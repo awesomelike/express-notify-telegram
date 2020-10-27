7d3654b3ae5ba2d3bc6da6749aebc14fe44d9305
awesomelike <abdullaev.dev@gmail.com>
2020-10-27 09:00:48 +0500

    notification template made less cluttered

--- src/templates/default.js
+++ src/templates/default.js
@@ -2,23 +2,23 @@ const escape = require('../utils/escaper');
 const statusCodeMapper = require('../utils/statusCodeMap');
 
 module.exports = (req, res, options) => {
-  const notificationTitle = res.statusCode < 500 ? '⚠️⚠️⚠️ SERVER WARNING ⚠️⚠️⚠️' : '⛔️⛔️⛔️ SERVER ERROR ⛔️⛔️⛔️';
+  const notificationTitle = res.statusCode < 500 ? '⚠️ SERVER WARNING 📡' : '⛔️ SERVER ERROR 📡';
   const endpoint = `\`${req.method}\` ${`${req.protocol}://${req.get('host')}${req.originalUrl}`}`;
   const appName = options.appName || process.env.APP_NAME || 'Unknown application';
 
   const message = `
   *${notificationTitle}*
     
-*🔺 APPLICATION:* ${escape(appName)}
+*🔧 APPLICATION:* ${escape(appName)}
     
-*🔺 ENDPOINT:* ${endpoint}
-*🔺 STATUS_CODE:* ${statusCodeMapper(res.statusCode)}
+*🔧 ENDPOINT:* ${endpoint}
+*🔧 STATUS_CODE:* ${statusCodeMapper(res.statusCode)}
     
-*🔺 ERROR_MESSAGE:* ${escape(req.errorMessage || 'Unknown')}
+*🔧 ERROR_MESSAGE:* ${escape(req.errorMessage || 'Unknown')}
     
-*🔺 IP_ADDRESS:* ${escape(req.ip)}
+*🔧 IP_ADDRESS:* ${escape(req.ip)}
     
-*🔺REQUEST_BODY:* \`\`\`${JSON.stringify(req.body || {}, null, 2)}\`\`\`
+*🔧 REQUEST_BODY:* \`\`\`${JSON.stringify(req.body || {}, null, 2)}\`\`\`
   `;
   return message;
 };
